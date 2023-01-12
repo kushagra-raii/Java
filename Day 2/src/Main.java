2491229e603605f2b9725d66856d0f3d79e433b7
kushagra-raii <kushagrarai83@gmail.com>
2023-01-12 22:26:15 +0530

    loops started

---
+++ Day 2/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+
+//        loops
+//        Problem 1 : Sum of n number
+//        int n = scr.nextInt();
+//        int sum=0;
+//        for (int i=1;i<=n;i++){
+//            sum = sum + i;
+//        }
+//        System.out.println(sum);
+
+//        problem 2 Sum stream of integer
+//        int n=0,sum=0;
+//        while (n!=-1){
+//            sum = sum +n;
+//            n = scr.nextInt();
+//        }
+//        System.out.println(sum);
+
+//        //    problem 3 : print first multiple of 5 && 7
+//        for (int i =1;true;i++){
+//            if (i%5==0 && i%7==0){
+//                System.out.println(i);
+//                break;
+//            }
+//        }
+
+//        Problem 4 : print no. from 1 to 50 except multiple of 3
+//        for (int i=1;i<=50;i++){
+//            if (i%3==0){
+//                continue;
+//            }
+//            else System.out.println(i);
+//        }
+
+//        problem 5 :count no. of digit in a number
+        int digit = scr.nextInt();
+
+
+
+    }
+}
